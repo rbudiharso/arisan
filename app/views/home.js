666568d1a24443bf47e8e5e4812bb5d33ce27ee4
Rahmat Budiharso <rbudiharso@gmail.com>
2015-06-22 16:56:01 +0700

    basic

--- app/views/home.js
+++ app/views/home.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Backbone = require('backbone');
+var insignia = require('insignia');
 
 function drawing(collection, callback, finishCallback) {
   var timeout = 0;
@@ -8,7 +9,7 @@ function drawing(collection, callback, finishCallback) {
 
   function run(time) {
     setTimeout(function() {
-      if (time < 500) {
+      if (time < 300) {
         selected = collection.draw();
         callback(null, selected);
         run(time+5);
@@ -25,18 +26,26 @@ var HomeView = Backbone.View.extend({
   render: function() {
     var template = require('../templates/home.hbs');
     this.$el.html(template());
+    this.insignia = insignia(this.$('#entries')[0], {
+      delimiter: ',',
+      deletion: true,
+    });
     return this;
   },
   events: {
     'click .draw': 'draw'
   },
   draw: function() {
+    var entries = this.insignia.tags();
+    this.collection.reset(entries.map(function(entry) {
+      return {name: entry};
+    }));
     var $result = this.$('.jumbotron h1');
     drawing(this.collection, function(err, selected) {
       $result.html(selected.get('name'));
     }, function(err, selected) {
-      $result.before('<h1>FINAL RESULT</h1>');
-      $result.html(selected.get('name'));
+      $result.before('<h3>FINAL RESULT</h3>');
+      $result.html(selected.get('name').toUppercase());
     });
   }
 });
